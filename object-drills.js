315ee940250227b531fa3abaa0aa56eef690f987
Adrian Ross <adriantoddross@gmail.com>
2018-01-17 12:41:14 -0500

    finished findById

--- object-drills.js
+++ object-drills.js
@@ -82,79 +82,46 @@
 //   return results;
 //   }
 
-const studentData = [
-  {
-    name: 'Tim',
-    status: 'Current student',
-    course: 'Biology',
-  },
-  {
-    name: 'Sue',
-    status: 'Withdrawn',
-    course: 'Mathematics',
-  },
-  {
-    name: 'Liz',
-    status: 'On leave',
-    course: 'Computer science',
-  },
+// const studentData = [
+//   {
+//     name: 'Tim',
+//     status: 'Current student',
+//     course: 'Biology',
+//   },
+//   {
+//     name: 'Sue',
+//     status: 'Withdrawn',
+//     course: 'Mathematics',
+//   },
+//   {
+//     name: 'Liz',
+//     status: 'On leave',
+//     course: 'Computer science',
+//   },
+// ];
+
+// function enrollInSummerSchool(students) {
+//   // your code here
+//   students.forEach( student => {
+//     student.status = "In Summer School";
+//   })
+// return students;
+// }
+
+// enrollInSummerSchool(studentData);
+
+// you can pass in `scratchData` to test out `findByid`
+// your function
+const scratchData = [
+  { id: 22, foo: 'bar' },
+  { id: 28, foo: 'bizz' },
+  { id: 19, foo: 'bazz' },
 ];
 
-function enrollInSummerSchool(students) {
-  // your code here
-  students.forEach( student => {
-    student.status = "In Summer School";
-  })
-return students;
-}
-
-enrollInSummerSchool(studentData);
-
-function testIt() {
-  var testData = [
-    {
-      name: 'Burt',
-      status: 'Playing hooky',
-      course: 'Biology',
-    },
-    {
-      name: 'Melanie',
-      status: 'Sick',
-      course: 'Mathematics',
-    },
-    {
-      name: 'Leonard',
-      status: 'AWOL',
-      course: 'Computer science',
-    },
-  ];
-
-  var results = enrollInSummerSchool(testData);
-
-  if (!(results && results instanceof Array)) {
-    console.error('FAILURE: `enrollSummerSchool` must return an array');
-    return;
+function findById(items, idNum) {
+  for (let i = 0; i < items.length; i++) {
+    if (idNum === items[i].id) {
+      return items[i];
+      }
+    }
   }
-  var result = testData.every(function(student) {
-    var match = results.find(function(_student) {
-      return (
-        _student.name === student.name &&
-        _student.course === student.course &&
-        _student.status.toLowerCase() === 'in summer school'
-      );
-    });
-    return match !== undefined;
-  });
-  if (!result) {
-    console.error(
-      'FAILURE: `enrollSummerSchool` should return ' +
-        'original key/value pairs for each student, and ' +
-        'update `status` to "In Summer school": ' +
-        JSON.stringify(results)
-    );
-  } else {
-    console.info('SUCCESS: `enrollSummerSchool` is working');
-  }
-}
-
-testIt();
